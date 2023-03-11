c44273c4a4ff816a5ebaabc1fcccf30563d75768
Alexander Pajer <alexander.pajer@integrations.at>
2023-03-11 09:32:44 +0100

    module added

--- demos/08-testing/01-jasmine/jasmine-testing/src/app/demos/samples/component-test/simple-food/simple-food.component.spec.ts
+++ demos/08-testing/01-jasmine/jasmine-testing/src/app/demos/samples/component-test/simple-food/simple-food.component.spec.ts
@@ -1,28 +1,37 @@
 import { of } from 'rxjs';
 import { foodData, serviceResult } from './simple-food-component.data';
 import { SimpleFoodComponent } from './simple-food.component';
+import { TestBed } from '@angular/core/testing';
+import { FoodServiceBS } from '../../food/food.service-bs';
+import { MatCardModule } from '@angular/material/card';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('Component - Spy - FoodComponent:', () => {
-  let comp: SimpleFoodComponent;
+  let component: SimpleFoodComponent;
   let fs: any;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [MatCardModule, NoopAnimationsModule],
+      declarations: [SimpleFoodComponent],
+      providers: [FoodServiceBS],
+    });
 
     fs = jasmine.createSpyObj(['getFood', 'deleteFood']);
     fs.getFood.and.returnValue(of(foodData))
-    comp = new SimpleFoodComponent(fs);
-    comp.ngOnInit()
+    component = new SimpleFoodComponent(fs);
+    component.ngOnInit()
   });
 
   it('should call getItems to subscribe data', () => {
-    expect(comp.food.length).toBe(4)
+    expect(component.food.length).toBe(4)
   })
 
   it('removes the item from the list', () => {
     fs.deleteFood.and.returnValue(of(serviceResult));
     const deletedFood = foodData[3];
-    comp.deleteFood(deletedFood);
-    expect(comp.food.length).toBe(3);
+    component.deleteFood(deletedFood);
+    expect(component.food.length).toBe(3);
   });
 
 });
