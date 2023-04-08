44f862cf6230364d45e6e5437dea4ebf9dc34b45
Alexander Pajer <alexander.pajer@integrations.at>
2023-04-08 17:26:37 +0200

    reusability - update nx sample Fixes #221

--- demos/08-reusability/01-libraries/ux-lib/projects/ux-controls/src/lib/ux-controls.module.ts
+++ demos/08-reusability/01-libraries/ux-lib/projects/ux-controls/src/lib/ux-controls.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { SplitComponent } from './controls/split/split.component';
 
-const comps = [SplitComponent];
-
 @NgModule({
-  declarations: [comps],
-  imports: [MatToolbarModule, FlexLayoutModule],
-  exports: comps,
+  declarations: [SplitComponent],
+  imports: [MatToolbarModule],
+  exports: [SplitComponent],
 })
-export class UxControlsModule {}
+export class UxControlsModule { }
--- demos/08-reusability/02-nx/tutorial-ws/apps/ng-otherapp/src/app/app.module.ts
+++ demos/08-reusability/02-nx/tutorial-ws/apps/ng-otherapp/src/app/app.module.ts
@@ -4,16 +4,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { UxControlsModule } from '@tutorial-ws/ux-controls';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
-    UxControlsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
