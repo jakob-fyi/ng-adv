e9be48bc500c9b8d6a7107f7e394ac3419c9df0f
Jakob Vesely <contact@jakob.fyi>
2024-05-13 16:23:38 +0200

    next

--- labs/lab-01/standalone-migration/migration-starter/src/app/app.config.ts
+++ labs/lab-01/standalone-migration/migration-starter/src/app/app.config.ts
@@ -1,53 +1,24 @@
-// import {
-//     HTTP_INTERCEPTORS,
-//     HttpClient,
-//     provideHttpClient
-// } from '@angular/common/http';
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { MarkdownModule } from 'ngx-markdown';
-// import { AppRoutingModule } from './app.routes';
-// import { AppComponent } from './app.component';
-// import { HomeComponent } from './home/home.component';
-// import { MaterialModule } from './material.module';
-// import { LoadingInterceptor } from './shared/loading/loading-interceptor';
-// import { LoadingService } from './shared/loading/loading.service';
-// import { SharedModule } from './shared/shared.module';
-
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
-import { LoadingService } from './shared/loading/loading.service';
-import { LoadingInterceptor } from './shared/loading/loading-interceptor';
+import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { MarkdownModule } from 'ngx-markdown';
 import { routes } from './app.routes';
-import { ApplicationConfig } from '@angular/core';
 
-// @NgModule({
-//     declarations: [AppComponent],
-//     imports: [
-//         BrowserModule,
-//         AppRoutingModule,
-//         BrowserAnimationsModule,
-//         MaterialModule,
-//         SharedModule,
-//         MarkdownModule.forRoot({
-//             loader: HttpClient,
-//         }),
-//         HomeComponent,
-//     ],
-//     providers: [
-//         provideHttpClient(),
-//         LoadingService,
-//         { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
-//     ],
-//     bootstrap: [AppComponent],
-// })
-// export class AppModule { }
 export const appConfig: ApplicationConfig = {
     providers: [
         provideHttpClient(),
         provideAnimations(),
-        provideRouter(routes)
+        provideRouter(routes),
+        {
+            provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+            useValue: {
+                appearance: 'outline',
+            },
+        },
+        importProvidersFrom([
+            MarkdownModule.forRoot()
+        ])
     ]
 }
