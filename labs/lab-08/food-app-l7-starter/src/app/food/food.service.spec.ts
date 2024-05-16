1ea5653305130581ecc8fc83bacfd6e34b1cd4e2
Vesely Jakob (LAD1-IT) <Jakob.Vesely@noel.gv.at>
2024-05-16 14:50:18 +0200

    done

--- labs/lab-08/food-app-l7-starter/src/app/food/food.service.spec.ts
+++ labs/lab-08/food-app-l7-starter/src/app/food/food.service.spec.ts
@@ -1,11 +1,46 @@
+import { TestBed } from '@angular/core/testing';
 import { FoodItem } from './food.model';
 import { FoodService } from './food.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { foodLoadData, foodQueryItem } from './food.mocks';
+import { environment } from 'src/environments/environment';
 
 describe('FoodService', () => {
   //add your setup here
+  let fs: FoodService;
+  let controller: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [FoodService],
+    });
+
+    fs = TestBed.inject(FoodService);
+    controller = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    controller.verify();
+  });
 
   it('should return the initial load data', () => {
-    pending();
+
+    fs.getFood().subscribe((data) => {
+      expect(data).toBeTruthy();
+      expect(data.length).toBe(foodLoadData.length);
+      const firstFood = data.find((f) => f.id == 2);
+      expect(firstFood).toEqual(foodQueryItem);
+    });
+
+    const url = `${environment.api}food`;
+    const req = controller.expectOne(url);
+    expect(req.request.method).toEqual('GET');
+    req.flush(foodLoadData);
+
   });
 
   it('should create a new food item', () => {
